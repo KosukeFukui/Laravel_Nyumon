37e297aa9d7489cb51dd8fb34a3f14ac442b386f
Kosuke Fukui <kosuke.fukui@agent-network.com>
2019-02-15 10:57:45 +0900

    Laravel_Nyumon_p315

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -12,8 +12,8 @@ use App\Person;
 class HelloController extends Controller {
     public function index(Request $request) {
         $sort = $request->sort;
-        $items = DB::table('people')->orderBy($sort, 'asc')->simplePaginate(5);
-        //$items = Person::orderBy($sort, 'asc')->simplePaginate(5);
+        //$items = DB::table('people')->orderBy($sort, 'asc')->paginate(5);
+        $items = Person::orderBy($sort, 'asc')->paginate(5);
         $param = ['items' => $items, 'sort' => $sort];
         return view('hello.index', $param);
     }
